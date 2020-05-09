a786eb399f46fafab1a566497e870ae1571c1f85
Dan Kessler <kesslerd@umich.edu>
2020-05-09 16:46:57 -0400

    increase resolution and simplify script

--- laura-cloud.py
+++ laura-cloud.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
@@ -26,22 +26,12 @@ text = text1 + text2 + text3
 # hand-lettered by Sara :)
 mask = np.array(Image.open(path.join(d, "mask.png")))
 
-stopwords = set(STOPWORDS)
-stopwords.add("said")
 
-wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
-               stopwords=stopwords, contour_width=3, contour_color='steelblue')
+wc = WordCloud(background_color="white", max_words=3000, mask=mask,
+               contour_width=3, contour_color='steelblue', width=1920, height=1080)
 
 # generate word cloud
 wc.generate(text)
 
 # store to file
-wc.to_file(path.join(d, "alice.png"))
-
-# show
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.figure()
-plt.imshow(alice_mask, cmap=plt.cm.gray, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+wc.to_file(path.join(d, "laura-raw.png"))
